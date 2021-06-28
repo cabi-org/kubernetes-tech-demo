25558dc9ba3f1bae744bdd267e9881dc14c8e8e7
Neil Docherty <N.Docherty@cabi.org>
2021-06-28 21:16:16 +0100

    Adding missing extensions

--- apply-configs.sh
+++ apply-configs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f td/configmap-withrefresh-$1
-kubectl apply -f td/configmap-withoutrefresh-$1
+kubectl apply -f td/configmap-withrefresh-$1.yaml
+kubectl apply -f td/configmap-withoutrefresh-$1.yaml
