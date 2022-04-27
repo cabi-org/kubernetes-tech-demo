d772ba243de534e86571b3a52c3892b08c2d168a
Neil Docherty <n.docherty@cabi.org>
2022-04-27 20:12:57 +0100

    Update to support name change

--- refresh.sh
+++ refresh.sh
@@ -1,1 +1,9 @@
-rm -R -f td && git clone https://github.com/cabi-org/kubernetes-tech-demo.git td && bash td/setup-td-scripts-and-value-files.sh your-name
+#!/bin/bash
+
+USER_NAME=$1
+
+if [ "$USER_NAME" == "" ]; then
+USER_NAME=your-name
+fi
+
+rm -R -f td && git clone https://github.com/cabi-org/kubernetes-tech-demo.git td && bash td/setup-td-scripts-and-value-files.sh "$USER_NAME"
