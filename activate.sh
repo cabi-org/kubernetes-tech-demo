e9e0502de8e9a884aa2ff7a4ca9b5e373fbb1997
Neil Docherty <n.docherty@cabi.org>
2022-04-27 16:53:02 +0100

    Add support for custom company name

--- activate.sh
+++ activate.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 ENVIR=$1
 USER_OR_ADMIN=$2
+COMPANY=$3
 
 if [ "$ENVIR" == "" ]; then
 ENVIR=internal
@@ -10,5 +11,9 @@ if [ "$USER_OR_ADMIN" == "" ]; then
 USER_OR_ADMIN=user
 fi
 
+if [ "$COMPANY" == "" ]; then
+COMPANY=cabi
+fi
+
 echo "Attempting to activate $USER_OR_ADMIN on $ENVIR cluster"
-cp -f ".kube/cabi-$ENVIR-$USER_OR_ADMIN" .kube/config
+cp -f ".kube/$COMPANY-$ENVIR-$USER_OR_ADMIN" .kube/config
