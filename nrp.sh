f550de462c6a819b8ed003af0bdbacafa6596bdf
Neil Docherty <n.docherty@cabi.org>
2022-05-10 12:31:45 +0100

    Add namespacing option to NRP

--- nrp.sh
+++ nrp.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+NAMESPACE=""
+
+while getopts ":n:" opt; do
+  case $opt in
+    n) NAMESPACE="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+if [ "$NAMESPACE" == "" ]; then
 kubectl get pods -A -o wide | grep -v Running
+else
+kubectl get pods -n $NAMESPACE -o wide | grep -v Running
+fi
