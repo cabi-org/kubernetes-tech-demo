97040e267d15629240193ab4d1a24957c78bdac2
Neil Docherty <n.docherty@cabi.org>
2022-05-14 15:28:25 +0100

    Add domain mappings

---
+++ domain-mappings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOMAIN=$1
+NAMESPACE=$2
+
+if [ "$NAMESPACE" == "" ]; then
+if [ "$DOMAIN" == development* ]; then
+NAMESPACE=development
+elif [ "$DOMAIN" == staging* ]; then
+NAMESPACE=staging
+elif [ "$DOMAIN" == *.cabisandbox.org ]; then
+NAMESPACE=sandbox-${DOMAIN/.cabisandbox.org/""}
+fi
+fi
+
+kubectl get -n $NAMESPACE ingress -o json | jq -r '.items[].spec.rules[] | select (.host == "$DOMAIN") | .http.paths[].path'
