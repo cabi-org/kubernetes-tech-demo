9471efc80ff021b5a9e0f7825c30c371a4465af3
Neil Docherty <n.docherty@cabi.org>
2022-05-30 20:42:23 +0100

    Basic markdown option added

--- all-domains.sh
+++ all-domains.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 NAMESPACE=$1
+MARKDOWN=$false
 if [ "$NAMESPACE" == "" ]; then
 NAMESPACE=development
+fi
+if [ "$2" == "md" ]; then
+MARKDOWN=$true
 fi
 
 echo
@@ -12,11 +16,20 @@ echo
 DOMAINS=$(kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select ((.http != null) ) | .host")
 DOMAINS=$(echo "$DOMAINS" | sort | uniq)
 
+if [ $MARKDOWN -eq $true ]; then
+echo "|Path|Application|"
+echo "|---|---|"
 while IFS= read -r DOMAIN; do
-echo $DOMAIN
+echo "|$DOMAIN|"
+kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select (.host == \"$DOMAIN\") | \"|\" + .http.paths[].path + \"|\" + .http.paths[].backend.service.name + \"|\""
+done <<< "$DOMAINS"
+else
+while IFS= read -r DOMAIN; do
+echo "$DOMAIN"
 kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select (.host == \"$DOMAIN\") | \" - \" + .http.paths[].path + \" [\" + .http.paths[].backend.service.name + \"]\""
 echo
 done <<< "$DOMAINS"
+fi
 
 echo
 echo "Redirects mapped from $NAMESPACE"
