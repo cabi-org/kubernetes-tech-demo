1d03f8d2a5bc39d2bb04ce3e8dfa89dd5b85c455
Neil Docherty <n.docherty@cabi.org>
2022-05-30 20:08:07 +0100

    Include domains without root site

--- all-domains.sh
+++ all-domains.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 NAMESPACE=$1
-
 if [ "$NAMESPACE" == "" ]; then
 NAMESPACE=development
 fi
@@ -10,11 +9,12 @@ echo
 echo "Domains mapped from $NAMESPACE"
 echo
 
-DOMAINS=$(kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select ((.http != null) and (.http.paths[].path == \"/\")) | .host")
+DOMAINS=$(kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select ((.http != null) ) | .host")
+DOMAINS=$(echo "$DOMAINS" | sort | uniq)
 
 while IFS= read -r DOMAIN; do
 echo $DOMAIN
-kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select (.host == \"$DOMAIN\") | \" - \" + .http.paths[].path"
+kubectl get ingress -n $NAMESPACE -o json | jq -r ".items[].spec.rules[] | select (.host == \"$DOMAIN\") | \" - \" + .http.paths[].path + \" [\" + .http.paths[].backend.service.name + \"]\""
 echo
 done <<< "$DOMAINS"
 
